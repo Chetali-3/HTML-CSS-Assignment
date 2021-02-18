84ff08055ac782207f4f5a82ac31c3c473111de4
github chetali-3 <vchetalibhatt@gmail.com>
2021-02-18 05:59:14 +0000

    Add CSS

---
+++ CSS.py
@@ -0,0 +1,44 @@
+# CSS codes of same assignment
+
+body{
+  background-color: lightcyan;
+ background-origin: 2cm;
+ margin:0.5cm;
+}
+img{
+  width : 5cm;
+  float: left;
+  margin-left: 0.5cm;
+}
+img5{
+  width :2cm;
+  float: right;
+  margin-right: 3cm;
+}
+h1 {
+  text-align: center;
+  color: darkmagenta;
+  font-family: cursive;
+  font-size: 50px;
+}
+h2 {
+  text-align: center;
+  color: darkviolet;
+  font-size: 30px;
+  font-family: cursive;
+}
+p{
+  text-align: center;
+  color: lightslategrey;
+  font-size: 18px;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  margin-top: 1cm;
+}
+a{
+  color: lightslategray;
+}
+li{
+  color: lightslategrey;
+  font-size: 20px;
+}
+
